19f4e8532a212726d8cb36e3c7377ae3bcf6b398
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-03-28 01:03:33 -0700

    Add files via upload

---
+++ ptree-private.h
@@ -0,0 +1,27 @@
+/*
+*  File:        ptree-private.h
+*  Description: Private functions for the PTree class
+*  Date:        2022-03-06 03:30
+*
+*               DECLARE YOUR PTREE PRIVATE MEMBER FUNCTIONS HERE
+*/
+
+#ifndef _PTREE_PRIVATE_H_
+#define _PTREE_PRIVATE_H_
+
+/////////////////////////////////////////////////
+// DEFINE YOUR PRIVATE MEMBER FUNCTIONS HERE
+//
+// Just write the function signatures.
+//
+// Example:
+//
+// Node* MyHelperFunction(int arg_a, bool arg_b);
+//
+/////////////////////////////////////////////////
+void Clear(Node* subroot);
+Node* copyhelper(Node* root);
+Node* Copy2( Node* otherRoot);
+HSLAPixel average(PNG& image, pair<unsigned int, unsigned int> ul, unsigned int w, unsigned int h);
+void deshelper(Node* root);
+#endif
